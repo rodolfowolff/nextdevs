a2bf4486b39cb65317ad812d92fa39d3e2b678d4
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-02-24 00:13:59 -0300

    feat: component tecnologies

---
+++ src/templates/Tecnologies/index.tsx
@@ -0,0 +1,16 @@
+import Header from '@components/Header'
+import { Base } from '../Base'
+
+const TecnologiesPage = () => {
+  return (
+    <Base>
+      <Header
+        title="Tecnologias"
+        subtitle="Xablau é um blog sobre desenvolvimento de software, 
+        com foco em tecnologias como React, Next.js, Node, GraphQL, Javascript e mais."
+      />
+    </Base>
+  )
+}
+
+export default TecnologiesPage
