7768a13ebeb8af9afa29bb9ac8165660bdfeb062
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-02-24 20:21:15 -0300

    feat: component heading

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,20 @@
+import styled, { css } from 'styled-components'
+
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.font.size.xlg};
+    font-weight: ${theme.font.weight.normal};
+    position: relative;
+    margin-bottom: calc(${theme.spacing.xsm} + 1.6rem);
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 25rem;
+      height: 0.3rem;
+      background-color: ${theme.color.primary};
+      left: 0;
+      bottom: -1.2rem;
+    }
+  `}
+`
