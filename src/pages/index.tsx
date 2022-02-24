2de0cdd0327dfb98bc044d5e279626d797be676b
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-02-24 00:12:57 -0300

    feat: page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,55 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps } from 'next'
 import Seo from '@components/seo/seo'
+import HomePage from 'src/templates/Home'
+import { client, ssrCache } from 'src/lib/urql'
+import {
+  PageDocument,
+  PostsDocument,
+  usePostsQuery,
+  usePageQuery,
+} from 'src/generated/graphql'
 
-const Home: NextPage = () => {
+export default function Home() {
+  const [
+    {
+      data: { posts },
+    },
+  ] = usePostsQuery()
+  const [
+    {
+      data: { page },
+    },
+  ] = usePageQuery({
+    variables: {
+      slug: 'home',
+    },
+  })
+
   return (
     <>
-      <Seo
-        title="Página inicial | NextDevs"
-        description="NextDevs é um blog sobre desenvolvimento de software, com foco em tecnologias como React, Next.js, Node, GraphQL, Javascript e outras tecnologias."
-        path="/"
-      />
-      <div>
-        <h1>conteudo do blog</h1>
-      </div>
+      {page && (
+        <>
+          <Seo
+            title={`${page?.title}`}
+            description={`${page?.subtitle}`}
+            path="/"
+          />
+          <HomePage page={page} posts={posts} />
+        </>
+      )}
     </>
   )
 }
 
-export default Home
+export const getServerSideProps: GetServerSideProps = async () => {
+  await Promise.all([
+    client.query(PostsDocument).toPromise(),
+    client.query(PageDocument, { slug: 'home' }).toPromise(),
+  ])
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  }
+}
