f1e6d03b10344f56584fe2931c06b4e5626a061f
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-02-24 00:13:48 -0300

    feat: component home

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,98 @@
+import Header from '@components/Header'
+import { MostPopular } from '@components/MostPopular'
+import { SectionMain } from '@components/SectionMain'
+import { Thumb } from '@components/Thumb'
+import { Base } from '../Base'
+import { Grid } from './styles'
+
+export type HomePageProps = {
+  page: {
+    content: {
+      text: string
+    }
+    subtitle: string
+    backgroundURL: string
+  }
+  posts: {
+    title: string
+    subtitle: string
+    image: string
+    slug: string
+    tags: string[]
+    coverImage: {
+      url: string
+    }
+  }[]
+}
+
+const SECTIONS_CONFIGS = [
+  {
+    tag: 'news',
+    size: 3,
+  },
+  {
+    tag: 'most-popular',
+    size: 8,
+  },
+  {
+    tag: 'latest',
+    size: 3,
+  },
+  // {
+  //   tag: 'recommended',
+  //   size: 6,
+  // },
+]
+
+export const HomePage = ({ page, posts }: HomePageProps) => {
+  return (
+    <>
+      <Base>
+        <Header {...page} />
+        <SectionMain>
+          {SECTIONS_CONFIGS.map(
+            section =>
+              section.tag === 'news' &&
+              posts
+                .slice(0, 1)
+                .filter(post => post.tags.includes('news'))
+                .map(post => (
+                  <Thumb
+                    key={post.slug}
+                    tagText={post.tags[0]}
+                    backgroundURL={post.coverImage.url}
+                    size="normal"
+                    orientation="landscape"
+                    slug={post.slug}
+                  />
+                ))
+          )}
+          <div className="right-side">
+            {SECTIONS_CONFIGS.map(
+              section =>
+                section.tag === 'news' &&
+                posts
+                  .slice(1, 3)
+                  .filter(post => post.tags.includes('news'))
+                  .map(post => (
+                    <Thumb
+                      key={post.slug}
+                      tagText={post.tags[0]}
+                      backgroundURL={post.coverImage.url}
+                      size="small"
+                      orientation="landscape"
+                      slug={post.slug}
+                    />
+                  ))
+            )}
+          </div>
+        </SectionMain>
+        <Grid>
+          <MostPopular posts={Object.values(posts)} />
+        </Grid>
+      </Base>
+    </>
+  )
+}
+
+export default HomePage
---
+++ src/templates/Home/styles.ts
@@ -0,0 +1,22 @@
+import styled, { css } from 'styled-components'
+
+const HEIGHT_PREVIEW = '15rem'
+const NUMBER_OF_ROWS = 5
+
+export const Grid = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: ${theme.spacing.xsm};
+
+    height: calc(
+      (${HEIGHT_PREVIEW} + ${theme.spacing.xsm}) * ${NUMBER_OF_ROWS}
+    );
+  `}
+
+  @media (max-width: 768px) {
+    display: block;
+    grid-template-columns: 1fr;
+    gap: 0;
+  }
+`
